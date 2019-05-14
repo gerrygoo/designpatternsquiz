215fefacffa5cb45512cdcb3ec1aaab7441bb569
gerry <gerrygoable@outlook.com>
2019-05-14 00:25:26 -0500

    100% documented all code is ugly AF now ):

--- src/models/api.rb
+++ src/models/api.rb
@@ -8,25 +8,44 @@
 require 'Faraday'
 require 'singleton'
 
-
+# The questions microservice endpoint url
 QUESTIONS_ENDPOINT = 'https://cxi64jj0b0.execute-api.us-east-2.amazonaws.com/default/quiz_questions'
+# The questions microservice api key
 QUESTIONS_KEY = 'jVv5JUY3R958HMEa27LWK4fDzHvBUKYx8hXHTRUK'
 
+# The score microservice endpoint url
 SCORE_ENDPOINT = 'https://t32wtpon7k.execute-api.us-east-2.amazonaws.com/default/quiz_score'
+# The score microservice api key
 SCORE_KEY = 'YtB1AtYNz3bU0fSEhI2q1lmECbdoZng22ZYeCs15'
 
+# The sms microservice endpoint url
 SMS_ENDPOINT = 'https://t32wtpon7k.execute-api.us-east-2.amazonaws.com/default/quiz_score'
+# The sms microservice api key
 SMS_KEY = 'jCI1WzYUnM7LxjbmjoF5HaC1Gz4gsfVN1T2VpC9C'
 
+# The +API+ is an implementation of the Singleton
+# pattern. It allows convenient access to our microservices.
 class API
     include Singleton
 
+    # Creates a new API object.
+    #
+    # Returns:: The new API object.
     def initialize
         @questions_conn = Faraday.new :url => QUESTIONS_ENDPOINT
         @score_conn = Faraday.new :url => SCORE_ENDPOINT
         @sms_conn = Faraday.new :url => SMS_ENDPOINT
     end
 
+    # Performs a get request with parameters on a given connection object
+    # using a given api key header.
+    #
+    # Parameter::
+    #   conn:: The connection object to use.
+    #   key:: The key sring to use as the +x-api-key+ header.
+    #   params:: Query parameters to send.
+    #
+    # Returns:: The request's response's body as a dictionary.
     def get_from conn, key, params = { }
         res = conn.get do | req |
             req.headers['x-api-key'] = key
@@ -35,6 +54,15 @@ class API
         JSON.parse res.body
     end
 
+    # Performs a post request with a given body on a given connection object
+    # using a given api key header.
+    #
+    # Parameter::
+    #   conn:: The connection object to use.
+    #   key:: The key sring to use as the +x-api-key+ header.
+    #   body:: Request body to send.
+    #
+    # Returns:: The request's response's body as a dictionary.
     def post_to conn, key, body = { }
         res = conn.post do | req |
             req.headers['x-api-key'] = key
@@ -44,18 +72,36 @@ class API
     end
 
 
+    # Gets a given number of random questions from the question microservice.
+    #
+    # Parameter::
+    #   number:: The quantity of questions to get.
+    #
+    # Returns:: An array of questions.
     def get_questions number = 5
         self.get_from @questions_conn, QUESTIONS_KEY, { count: number }
     end
 
+    # Gets the game's scoreboard from the scores microservice.
+    #
+    # Returns:: An array of scores.
     def get_scores
         self.get_from @score_conn, SCORE_KEY
     end
 
+    # Posts a name, score pair to the scores microservice.
+    #
+    # Returns:: The microservice's response.
     def post_score initials, score = 0
         self.post_to @score_conn, SCORE_KEY, JSON.generate({ initials: initials, score: score })
     end
 
+    # Posts an sms requesr to the sms microservice.
+    # Parameter::
+    #   phoneNumber:: The phone number to send stuff to.
+    #   message:: The message sring to send.
+    #
+    # Returns:: The microservice's response.
     def post_sms phoneNumber, message
         self.post_to @sms_conn, SMS_KEY, JSON.generate({ phoneNumber: phoneNumber, message: message })
     end
--- src/models/quiz.rb
+++ src/models/quiz.rb
@@ -7,7 +7,17 @@
 
 require 'json'
 
+# The +Quiz+ class is a 'data class' that models a quiz game.
 class Quiz
+
+    # Constructs a new Quiz object with either provided data or default data
+    # the member data is ever-wrapped in an associative array for easy JSON
+    # serialization
+    #
+    # Parameter::
+    #   data:: The quiz data the new object will model.
+    #
+    # Returns:: A new quiz object.
     def initialize data = { }
         with_defaults = {
             user_name: data["user_name"] || data[:user_name] || 'no_name',
@@ -18,33 +28,49 @@ class Quiz
         @data = with_defaults
     end
 
-
+    # Convenient accessor for the classes' data user_name field
     def user_name
         @data[:user_name]
     end
 
+    # Convenient accessor for the classes' data questions field
     def questions
         @data[:questions]
     end
 
+    # Convenient accessor for the classes' data progress field
     def progress
         @data[:progress]
     end
 
+    # Convenient accessor for the classes' data score field
     def score
         @data[:score]
     end
 
+    # Convenient accessor for the classes' data done field
     def done
         @data[:progress] >= @data[:questions].length
     end
 
-
+    # Creates a new greeter object.
+    #
+    # Parameter::
+    #
+    # Returns:: The last not-solved question.
+    #
+    # Raises:: +Something+ if +self.done+ or there are not any questions.
     def current_question
         raise 'quiz: current_question: no question available' if ( self.done or @data[:questions].empty? )
         @data[:questions][@data[:progress]]
     end
 
+    # Answers the current question.
+    #
+    # Parameter::
+    #   answer:: The answer to the question
+    #
+    # Returns:: True if the question was answered correctly, False otherwise.
     def answer_question answer
         correct_answer = self.current_question["answer"]
         was_answer_correct = answer == correct_answer
@@ -53,11 +79,16 @@ class Quiz
         was_answer_correct
     end
 
-
+    # Computes the quiz' string representation.
+    #
+    # Returns:: the quiz' string representation.
     def to_s
         self.to_json
     end
 
+    # Computes the quiz' JSON representation.
+    #
+    # Returns:: the quiz' JSON representation.
     def to_json
         @data.to_json
     end
